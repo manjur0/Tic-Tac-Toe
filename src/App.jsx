899e7b102405c258f1328463cbb4e587c382b61b
manjur0 <sfmanjur21@gmail.com>
2024-01-16 07:32:51 +0600

    Tic tac toe game made by manjur

---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+/* eslint-disable react/prop-types */
+
+import { useState } from "react";
+
+// Square component that renders a button
+function Square({ value, onSquareClick }) {
+  return (
+    <button
+      onClick={onSquareClick}
+      className="bg-white text-xl font-bold border-2  border-gray-500 h-12 w-12 gap-3 leading-9 m-2"
+    >
+      {value}
+    </button>
+  );
+}
+
+// board component that renders 3x3 squares
+function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  // function that handles the  winner
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  // function that handles the click event
+  const onSquareClick = (index) => {
+    if (squares[index] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[index] = "X";
+    } else {
+      nextSquares[index] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  };
+  return (
+    <>
+      <div className="text-xl font-bold">{status}</div>
+      <div className="flex">
+        <Square value={squares[0]} onSquareClick={() => onSquareClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => onSquareClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => onSquareClick(2)} />
+      </div>
+      <div className="flex">
+        <Square value={squares[3]} onSquareClick={() => onSquareClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => onSquareClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => onSquareClick(5)} />
+      </div>
+      <div className="flex">
+        <Square value={squares[6]} onSquareClick={() => onSquareClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => onSquareClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => onSquareClick(8)} />
+      </div>
+    </>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export default Board;
